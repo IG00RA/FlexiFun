d4b34ec26e904aede6c132ea077f37afff660fd3
Igor Gromadskiy <gromadskiyigor@gmail.com>
2024-12-10 19:23:52 +0200

    [Add] about desk styles

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -9,25 +9,27 @@ export default function About() {
   return (
     <section id="about" className={styles.about}>
       <div className={styles.imgWrap}></div>
-      <h2 className={styles.header}>
-        O <CSvg />
-        om je tento súbor?
-      </h2>
-      <p className={styles.text}>Fascinujúca interaktívna hra - rozprávka</p>
-      <ul className={styles.list}>
-        {aboutItems.map((item, index) => (
-          <li key={index}>
-            <div className={styles.iconWrap}>
-              <Icon name="icon-arrow_r" width={16} height={16} />
-            </div>
-            <p className={styles.par}>{item}</p>
-          </li>
-        ))}
-      </ul>
-      <Button>
-        Objedna
-        <TSvgMedium color="#ffffff" /> sadu
-      </Button>
+      <div className={styles.wrapper}>
+        <h2 className={styles.header}>
+          O <CSvg />
+          om je tento súbor?
+        </h2>
+        <p className={styles.text}>Fascinujúca interaktívna hra - rozprávka</p>
+        <ul className={styles.list}>
+          {aboutItems.map((item, index) => (
+            <li key={index}>
+              <div className={styles.iconWrap}>
+                <Icon name="icon-arrow_r" width={16} height={16} />
+              </div>
+              <p className={styles.par}>{item}</p>
+            </li>
+          ))}
+        </ul>
+        <Button>
+          Objedna
+          <TSvgMedium color="#ffffff" /> sadu
+        </Button>
+      </div>
     </section>
   );
 }
