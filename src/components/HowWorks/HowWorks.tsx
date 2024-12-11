6734ad2e30cc9d3dc4d8a7e7c9e6c5b75297aef0
Igor Gromadskiy <gromadskiyigor@gmail.com>
2024-12-11 14:06:24 +0200

    [Add] howWorks desk section

--- src/components/HowWorks/HowWorks.tsx
+++ src/components/HowWorks/HowWorks.tsx
@@ -9,26 +9,30 @@ export default function HowWorks() {
   return (
     <section className={styles.howWorks}>
       <h2 className={styles.header}>Ako to funguje</h2>
-      <div className={styles.imgWrap}></div>
-      <div className={styles.topWrap}>
-        <span className={styles.numbWrap}>4</span>
-        <h3 className={styles.topText}>
-          Kroky k pohlcujúcemu a pútavému učeniu
-        </h3>
+      <div className={styles.wrapper}>
+        <div className={styles.imgWrap}></div>
+        <div>
+          <div className={styles.topWrap}>
+            <span className={styles.numbWrap}>4</span>
+            <h3 className={styles.topText}>
+              Kroky k pohlcujúcemu a pútavému učeniu
+            </h3>
+          </div>
+          <ul className={styles.list}>
+            {howWorksItems.map((item, index) => (
+              <li key={index}>
+                <span className={styles.numbWrapList}>{item.numb}</span>
+                <div>
+                  <h3 className={styles.parHead}>{item.header}</h3>
+                  <p className={styles.par}>
+                    {replaceSymbol(item.text, { t: TSvgSmall })}
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <ul className={styles.list}>
-        {howWorksItems.map((item, index) => (
-          <li key={index}>
-            <span className={styles.numbWrapList}>{item.numb}</span>
-            <div>
-              <h3 className={styles.parHead}>{item.header}</h3>
-              <p className={styles.par}>
-                {replaceSymbol(item.text, { t: TSvgSmall })}
-              </p>
-            </div>
-          </li>
-        ))}
-      </ul>
       <Button>
         Objedna
         <TSvgMedium color="#ffffff" /> sadu
